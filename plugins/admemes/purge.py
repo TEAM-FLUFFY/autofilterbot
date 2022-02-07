ca0ab0b6953576f886c584d04ac36f3a91a56711
TEAM-FLUFFY <98939932+TEAM-FLUFFY@users.noreply.github.com>
2022-02-07 16:22:34 +0530

    Update purge.py

--- plugins/admemes/purge.py
+++ plugins/admemes/purge.py
@@ -5,7 +5,7 @@ import asyncio
 from pyrogram import Client, filters
 
 from plugins.helper_functions.admin_check import admin_check
-from plugins.helper_functions.cust_p_filters import f_onw_fliter
+from plugins.helper_functions.admim_check import admin_check
 
 TG_MAX_SELECT_LEN = 100
 
